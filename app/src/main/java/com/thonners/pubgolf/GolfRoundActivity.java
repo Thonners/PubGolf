6e65d81517d0f2bee2233ac58f9d58d712153536
Mathonwy Thomas <mathonwy.thomas@gmail.com>
2017-04-22 21:13:27 +0100

    #38: Override onBackPressed() in GolfRoundActivity to make a back press on the mapFragment take you back to the scorecard fragment.

--- app/src/main/java/com/thonners/pubgolf/GolfRoundActivity.java
+++ app/src/main/java/com/thonners/pubgolf/GolfRoundActivity.java
@@ -80,6 +80,8 @@ public class GolfRoundActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
+        } else if (viewPager.getCurrentItem() > SCORECARD_FOOTER_BUTTON_ID) {
+            showScorecard();
         } else {
             super.onBackPressed();
         }
